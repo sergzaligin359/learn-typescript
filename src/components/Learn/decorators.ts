55f7a765e683a7afc0cd652bfdd4c0f7a7e25660
sergzaligin359 <webdevjsphp@gmail.com>
2021-01-06 14:40:57 +0300

    fix

--- src/components/Learn/decorators.ts
+++ src/components/Learn/decorators.ts
@@ -1,3 +1,65 @@
 export default () => {
-  console.log('arr', arr);
+  //   function Log(constructor: Function) {
+  //     console.log('constructor', constructor);
+  //   }
+  //   function Log2(target: any, propName: string | symbol) {
+  //     console.log('target', target);
+  //     console.log('propName', propName);
+  //   }
+  //   function Log3(target: any, propName: string | symbol, descriptor: PropertyDescriptor) {
+  //     console.log('target', target);
+  //     console.log('propName', propName);
+  //     console.log('descriptor', descriptor);
+  //   }
+  //   @Log
+  //   class ComponentLog {
+  //     @Log2
+  //     name: string;
+  //     constructor(name: string) {
+  //       this.name = name;
+  //     }
+  //     @Log3
+  //     get componentName() {
+  //       return this.name;
+  //     }
+  //     @Log3
+  //     logName(): void {
+  //       console.log('Log name prop', this.name);
+  //     }
+  //   }
+  interface IComponentDecorator {
+    selector: string;
+    template: string;
+  }
+  function Component(config: IComponentDecorator) {
+    return <T extends new (...args: any[]) => object>(Constructor: T) => {
+      // tslint:disable-next-line: new-parens
+      return new (class extends Constructor {
+        constructor(...args: any[]) {
+          super(...args);
+          const card = document.querySelector(config.selector) as HTMLElement;
+          card.innerHTML = config.template;
+        }
+      })();
+    };
+  }
+  // tslint:disable-next-line: max-classes-per-file
+  @Component({
+    selector: '#card',
+    template: `
+      <div class="card">
+        <div class="card-content">
+          <span class="card-title"> Title component </span>
+        </div>
+      </div>
+    `,
+  })
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  class CardComponent {
+    constructor(public name: string) {}
+
+    logName(): void {
+      console.log('Log name prop');
+    }
+  }
 };
--- src/components/Learn/index.ts
+++ src/components/Learn/index.ts
@@ -1,13 +1,13 @@
-import reloadFunc from './reloadfunc';
-import types from './types';
-import arrays from './arrays';
-import classes from './classes';
-import generics from './generics';
+// import reloadFunc from './reloadfunc';
+// import types from './types';
+// import arrays from './arrays';
+// import classes from './classes';
+// import generics from './generics';
 import decorators from './decorators';
 
 export default (): void => {
   console.log('Learn...');
-  console.log('Reload func', reloadFunc(1, 99));
+  // console.log('Reload func', reloadFunc(1, 99));
   // types();
   // classes();
   // arrays();
